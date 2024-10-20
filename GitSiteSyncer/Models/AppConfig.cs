5bfce8073b215cddb0f612634f4e724d48e3b5c0
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-10-20 15:23:29 -0400

    enable deletion of files for sync

--- GitSiteSyncer/Models/AppConfig.cs
+++ GitSiteSyncer/Models/AppConfig.cs
@@ -9,5 +9,10 @@
         public int DaysToConsider { get; set; }
         public string AppHostDomain { get; set; } // New host domain for "app-link"
         public string NoAppHostDomain { get; set; } // New host domain for "no-app-link"
+
+        /// <summary>
+        /// List of HTML files to exclude from deletion (e.g., "404.html", "offline.html").
+        /// </summary>
+        public List<string> Exclusions { get; set; } = new List<string>();
     }
 }
